157ac4125ce3ae3d57a64981c1358ef74f9e9ab3
smyy325 <sumeyyea325@gmail.com>
2025-06-09 14:08:08 +0300

    Onboarding pages

---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,78 @@
+import 'package:farm/service/widget_support.dart';
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFFBF0),
+      body: Column(
+        children: [
+          const SizedBox(height: 40),
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // Gölgelendirme sadece resim için
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(90),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.8),
+                        spreadRadius: 12,
+                        blurRadius: 30,
+                        offset: Offset(0, 15),
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(90),
+                    child: Image.asset("assets/onboard.png"),
+                  ),
+                ),
+                const SizedBox(height: 60),
+                Text(
+                  "Nature’s Best,\nStraight to You",
+                  textAlign: TextAlign.center,
+                  style: AppWidget.HeadlineTextFeildStyle(),
+                ),
+                SizedBox(height: 15,),
+                Text("Craving fresh products straight from the farm?\nFarmly is here for you!",
+                  textAlign: TextAlign.center,
+                  style: AppWidget.SimpleTextFeildStyle(),
+                ),
+                SizedBox(height: 30.0,),
+                Container(
+                  height: 70,
+                  width: MediaQuery.of(context).size.width / 2,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF33691E),
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Get Started",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/service/widget_support.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppWidget{
+  static TextStyle HeadlineTextFeildStyle(){
+    return TextStyle(
+      color: Colors.black, fontSize: 35.0, fontWeight: FontWeight.bold
+    );
+  }
+
+  static TextStyle SimpleTextFeildStyle(){
+    return TextStyle(
+        color: Colors.black, fontSize: 20.0,
+    );
+  }
+}
